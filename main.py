57edd273187d6bf87a08d345afb73363ae1ecb06
Grant Mathews <johnfn@gmail.com>
2013-02-04 20:32:44 -0800

    Continually reload songs. It's a radio!
    Sometimes there's a long gap though, looks like mp3length.exe is wrong
    sometimes :/

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import urllib2
 import re
 import os
 import subprocess
+import time
 
+# Given the HTML for a OHC page, attempts to grab the theme from that page.
+# Might fail on weird weeks.
 def find_theme(html):
     theme = re.findall('(Tonights|Tonight\'s|Todays|Today\'s) theme is:? "(.*)"', html)
     if len(theme) > 0:
         return theme[0][1]
     return "???"
 
+# Given an OHC # and a ranking from the top of the page, returns the path to
+# the song at that rank.
 def get_ohc_url(which_ohc, which_rank):
     idx = 0
 
@@ -37,25 +42,28 @@ def get_ohc_url(which_ohc, which_rank):
         if x == which_rank:
             return url
 
+# Randomly selects and downloads an OHC song to disk, returning the path of that song
+def download_random_song():
+    ohc_num = str(int(random.random() * 225)).zfill(3)
+    song_rank = int(random.random() * 5)
+    
+    # Download the song
+    print "Downloading..."
+    req = urllib2.Request(get_ohc_url(ohc_num, song_rank))
+    mp3response = urllib2.urlopen(req)
+    
+    # write the mp3 out to disk
+    print "Saving..."
+    data = mp3response.read()
+    name = "ohc%s-%d.mp3" % (ohc_num, song_rank)
+    song = open(name, "wb")
+    song.write(data)
+    song.close()
+    
+    return name
 
-ohc_num = str(int(random.random() * 225)).zfill(3)
-song_rank = int(random.random() * 5)
-
-# Download the song
-print "Downloading..."
-req = urllib2.Request(get_ohc_url(ohc_num, song_rank))
-mp3response = urllib2.urlopen(req)
-
-# write the mp3 out to disk
-print "Saving..."
-data = mp3response.read()
-name = "ohc%s-%d.mp3" % (ohc_num, song_rank)
-song = open(name, "wb")
-song.write(data)
-song.close()
-
-print subprocess.check_output(["./mp3length.exe", name])
-
-os.startfile(name)
-
-
+# fire up the radio!
+while True:
+    song = download_random_song()
+    os.startfile(song)
+    time.sleep(int(subprocess.check_output(["./mp3length.exe", song])) + 3)
