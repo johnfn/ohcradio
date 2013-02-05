0e43b93a241c195364e8b667fcff4a130fa99de8
Grant Mathews <johnfn@gmail.com>
2013-02-04 18:21:01 -0800

    Fix bugs and get mp3 length.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import urllib2
 import re
 import os
+import subprocess
 
 def find_theme(html):
     theme = re.findall('(Tonights|Tonight\'s|Todays|Today\'s) theme is:? "(.*)"', html)
@@ -13,7 +14,7 @@ def get_ohc_url(which_ohc, which_rank):
     idx = 0
 
     # Download OHC page
-    response = urllib2.urlopen('http://compo.thasauce.net/rounds/view/OHC%s' % ohcnum)
+    response = urllib2.urlopen('http://compo.thasauce.net/rounds/view/OHC%s' % which_ohc)
     html = response.read()
     
     print "OHC%s, rank %d (Theme: %s)" % (which_ohc, which_rank + 1, find_theme(html))
@@ -33,25 +34,27 @@ def get_ohc_url(which_ohc, which_rank):
         idx = html.find('a href="/files/', idx + 1)
         endidx = html.find('">Download', idx)
         url = "http://compo.thasauce.net" + html[idx + len('a href="'):endidx ]
-        if x == song_place:
+        if x == which_rank:
             return url
 
 # Download the song
 
-ohcnum = str(int(random.random() * 225)).zfill(3)
-song_place = int(random.random() * 5)
+ohc_num = str(int(random.random() * 225)).zfill(3)
+song_rank = int(random.random() * 5)
 
 print "Downloading..."
-req = urllib2.Request(get_ohc_url(ohcnum, song_place))
+req = urllib2.Request(get_ohc_url(ohc_num, song_rank))
 mp3response = urllib2.urlopen(req)
 
 # write the mp3 out to disk
 print "Saving..."
 data = mp3response.read()
-name = "ohc%s-%d.mp3" % (ohcnum, 1)
+name = "ohc%s-%d.mp3" % (ohc_num, song_rank)
 song = open(name, "wb")
 song.write(data)
 song.close()
+
+print subprocess.check_output(["./mp3length.exe", name])
 
 os.startfile(name)
 
